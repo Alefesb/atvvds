faab97f39e981028ddb3d31e4be353e1a33dbe9b
Alefesb <142458915+Alefesb@users.noreply.github.com>
2024-09-06 20:42:47 -0300

    att 06/09

--- atvdd5/index.php
+++ atvdd5/index.php
@@ -7,9 +7,34 @@
 </head>
 <body>
     <?php
+    $menu = $_GET["menu"];
+    $saldo = 100;
+    $deposito = 0;
     
-    $saldo = $_GET;
-    $deposito = $
+    $sair = $_GET["sair"] ?? "saldo"; 
+    $value = $_GET["value"];
+
+    switch($menu) {
+        case 1 ;
+        echo ($saldo);
+        break;
+
+        case 2 ;
+         $saldo = $saldo + $value;
+        echo ($saldo);
+        break;
+
+        case 3 ;
+         $saldo = $saldo - $value;
+        echo ($saldo);
+        break;
+
+        case 4 ;
+        
+        echo ("Saindo...");
+
+        break;
+    }
 
     ?>
 </body>
